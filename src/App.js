d3829aefc6d263e5365908780a0d48c61d1ff93a
bauti.iglesias@gmail.com <bauti.iglesias@gmail.com>
2022-09-30 15:08:07 -0300

    initial header navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import './App.css';
+// import './App.css';
+import './App.scss';
 import Home from './components/pages/home/Home';
 import Register from './components/pages/register/Register';
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Header from './components/other/header/Header';
+import NotFound from './components/pages/404/NotFound';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home/>,
+    errorElement: <NotFound />,
+  },
+  {
+    path: "/register",
+    element: <Register/>,
   },
 ]);
 
