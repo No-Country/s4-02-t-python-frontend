cafa8d5a05d5ba4b56bf7e5e28d6248d241544cf
bauti.iglesias@gmail.com <bauti.iglesias@gmail.com>
2022-09-30 17:15:02 -0300

    header routing

--- src/App.js
+++ src/App.js
@@ -3,27 +3,36 @@ import './App.scss';
 import Home from './components/pages/home/Home';
 import Register from './components/pages/register/Register';
 
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from './components/other/header/Header';
 import NotFound from './components/pages/404/NotFound';
+import Login from './components/pages/login/Login';
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home/>,
-    errorElement: <NotFound />,
-  },
-  {
-    path: "/register",
-    element: <Register/>,
-  },
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Home/>,
+//     errorElement: <NotFound />,
+//   },
+//   {
+//     path: "/register",
+//     element: <Register/>,
+//   },
+// ]);
 
 function App() {
   return (
     <div class="App">
-      <Header/>
-      <RouterProvider router={router} />
+      <Router>
+        <Header/>
+        <hr />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
