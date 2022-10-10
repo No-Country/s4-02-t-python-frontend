25bacaf829e17cd365463e57be9551b1b1679d9d
bauti.iglesias@gmail.com <bauti.iglesias@gmail.com>
2022-10-10 20:21:29 -0300

    footer styling

--- src/components/other/footer/Footer.jsx
+++ src/components/other/footer/Footer.jsx
@@ -1,31 +1,38 @@
 import React from 'react';
-import brandImg from '../../../assets/isotipo.png';
+import './footer.scss';
+import brandImg from '../../../assets/footer/isologotipo.png';
 
 export default function Footer() {
   return (
     <footer className='container mx-auto'>
         <div className='row'>
-            <div className='col'>
-                <img src={brandImg} alt="" />
+            <div className='col my-auto text-center'>
+                <img src={brandImg} alt="" className='w-50' />
             </div>
             <div className='col'>
                 <strong>Acerca de</strong>
-                <p>POLITICAS DE COOKIES</p>
-                <p>CONTRATACION</p>
-                <p>CONDICIONES DE USO</p>
+                <div className='container-links mt-3'>
+                    <p>POLITICAS DE COOKIES</p>
+                    <p>CONTRATACION</p>
+                    <p>CONDICIONES DE USO</p>
+                </div>
             </div>
             
             <div className='col'>
                 <strong>Ayuda</strong>
-                <p>SOPORTE</p>
-                <p>CONTACTANOS</p>
-                <p>SUCURSALES</p>
+                <div className='container-links mt-3'>
+                    <p>SOPORTE</p>
+                    <p>CONTACTANOS</p>
+                    <p>SUCURSALES</p>
+                </div>
             </div>
             <div className='col'>
                 <strong>Nuestras redes</strong>
-                <p>FACEBOOK</p>
-                <p>TWITTER</p>
-                <p>INSTAGRAM</p>
+                <div className='container-links mt-3'>
+                    <p>FACEBOOK</p>
+                    <p>TWITTER</p>
+                    <p>INSTAGRAM</p>
+                </div>
             </div>
         </div>
     </footer>
